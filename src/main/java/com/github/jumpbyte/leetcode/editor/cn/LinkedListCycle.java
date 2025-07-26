f03924a55da653f17e15aa7cc82e59e73b9aa24c
yuanjinan <yuanjinan@daojia-inc.com>
2025-07-26 08:04:26 +0800

    1

---
+++ src/main/java/com/github/jumpbyte/leetcode/editor/cn/LinkedListCycle.java
@@ -0,0 +1,33 @@
+package com.github.jumpbyte.leetcode.editor.cn;
+
+import java.util.*;
+import com.github.jumpbyte.leetcode.editor.common.*;
+
+public class LinkedListCycle {
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    /**
+     * Definition for singly-linked list.
+     * class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) {
+     *         val = x;
+     *         next = null;
+     *     }
+     * }
+     */
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    
+    public static void main(String[] args) {
+        Solution solution = new LinkedListCycle().new Solution();
+        // put your test code here
+        
+    }
+}
---
+++ src/main/java/com/github/jumpbyte/leetcode/editor/cn/SortList.java
@@ -0,0 +1,32 @@
+package com.github.jumpbyte.leetcode.editor.cn;
+
+import java.util.*;
+import com.github.jumpbyte.leetcode.editor.common.*;
+
+public class SortList {
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode sortList(ListNode head) {
+            
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    
+    public static void main(String[] args) {
+        Solution solution = new SortList().new Solution();
+        // put your test code here
+        
+    }
+}
